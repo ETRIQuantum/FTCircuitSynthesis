3c1c6140c6006281f54061d8acf21f26a8a11052
ETRIQuantum <116781734+ETRIQuantum@users.noreply.github.com>
2022-11-01 09:13:30 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,30 @@
 # -*-coding:utf-8-*-
 
-# This code is part of ftsynthesis (fault-tolerant quantum circuit synthesis for fault-tolerant quantum protocols)
+
+# This code is part of ftsynthesis
+# (fault-tolerant quantum circuit synthesis for fault-tolerant quantum protocols)
 #
 # Copyright 2022 ETRI
 #
 # This code is licensed under the BSD-3-Clause.
 
+'''
+    setup
+'''
 
-
-import io
 from setuptools import find_packages, setup
 
 setup(
-	name				= "ftsynthesis",
-	version				= '0.0.2',
-	description			= 'fault tolerant circuit synthesis for universal fault-tolerant quantum computing based on concatenated codes',
-	author 				= 'Yongsoo Hwang',
-	author_email 		= 'yhwang@etri.re.kr',
-	install_requires 	= ['simplejson', 'icecream', 'pandas', 'networkx', 'parse', 'progress', 'qubitmapping', 'userproperty'],
-	packages 			= find_packages(),
-	zip_safe 			= False,
-	python_requires 	= '>=3'
-	)
+    name                = "ftsynthesis",
+    version             = '0.0.2',
+    description         = '''fault tolerant circuit synthesis
+                             for universal fault-tolerant quantum computing
+                             based on concatenated codes''',
+    author              = 'Yongsoo Hwang',
+    author_email        = 'yhwang@etri.re.kr',
+    install_requires    = ['simplejson', 'icecream', 'pandas', 'networkx',
+                           'parse', 'progress', 'qubitmapping', 'userproperty'],
+    packages            = find_packages(),
+    zip_safe            = False,
+    python_requires     = '>=3'
+    )
